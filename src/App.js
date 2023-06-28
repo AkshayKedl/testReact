95f1747dd1b5716247aae7e301b4cbe99b250b2f
Akshay Kedlaya <akshaykedlaya@divum.in>
2023-06-28 13:07:52 +0530

    added ga measurement ID

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(()=> {
+    ReactGA.initialize("G-BYLBJ952VH");
+  },[])
   return (
     <div className="App">
       <header className="App-header">
